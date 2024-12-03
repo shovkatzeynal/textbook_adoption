86fb39357f9f4d2851355e1afb1b22d47f7bc990
Shovkat Zeynal <sovketzeynalova@gmail.com>
2024-12-02 23:56:45 -0600

    db password added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "password", // Change as per your setup
-  database: "textbook_adopt_mvsu",
+  user: "root", // Update with your MySQL username
+  password: "Zeynalli...2000", // Update with your MySQL password
+  database: "textbook_adopt_mvsu", // Ensure this matches your schema name
 });
 
-module.exports = pool.promise();
+module.exports = db;
